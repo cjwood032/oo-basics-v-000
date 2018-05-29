1cf8373a7d66547b4dcc40e99fcd001eadd7b97a
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-29 07:50:16 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,12 @@ class Shoe
     @brand=brand
   end
   def cobble
+<<<<<<< HEAD
     puts "Your shoe is as good as new!"
     @condition="new"
+=======
+    puts "The shoe has been repaired"
+    @condition=new
+>>>>>>> 02c57cc13b2d2f3a7f612aca4b610dc98f01696e
   end
 end
